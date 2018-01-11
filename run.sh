c50e72234e52a5b49e12b730bf9e644f6ed43198
Sébastien Joly <joly.sebastien@gmail.com>
2018-01-11 15:18:35 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Run command for docker service fhem and sshd
-
+usr/bin/supervisord
 # check if ssh-keys exists 
 test -x /etc/ssh/ssh_host_dsa_key || dpkg-reconfigure openssh-server
 /etc/init.d/ssh start
