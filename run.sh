dc440c5332ddcca7b6c37d939d46529aad434de5
Sébastien Joly <joly.sebastien@gmail.com>
2018-01-11 10:24:38 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,12 +4,3 @@
 # check if ssh-keys exists 
 test -x /etc/ssh/ssh_host_dsa_key || dpkg-reconfigure openssh-server
 /etc/init.d/ssh start
-echo "Current directory : $(pwd)"
-echo "Environment RUNVAR: $RUNVAR"
-echo "There are $# arguments: $@"
-
-# make pidfile rundir for fhem (because /var/run  ist tmpfs in ram)
-if [ ! -d /var/run/fhem  ]; then
-    mkdir /var/run/fhem
-    chown -R fhem:root /var/run/fhem
-fi
